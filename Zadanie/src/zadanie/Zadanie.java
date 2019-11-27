910f6fd340f5582fdb16c67cd72fe2a1a1e0f210
noliosahrak <wsburghardt@gmail.com>
2019-11-27 11:15:03 +0100

    Liczenie słów działa poprawnie

--- Zadanie/src/zadanie/Zadanie.java
+++ Zadanie/src/zadanie/Zadanie.java
@@ -15,7 +15,22 @@ public class Zadanie {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String s = " Raz dwa     trzy    ghj ";
+        int wynik = liczenie(s);
+        System.out.println(wynik);
     }
     
+    public static int liczenie(String s) {
+        int ls = 1; //licznik słów
+        char poprzedni = ' ';
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ' && poprzedni != ' ') {
+                ls++;
+            }
+            poprzedni = c;
+        }
+        if (s.charAt(s.length() - 1) == ' ') ls--;
+        return ls;
+    }
 }
